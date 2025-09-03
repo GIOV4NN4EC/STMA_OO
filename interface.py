04af910e6bac2fe0898c04dc0c49cee06fd66f9f
Giovanna Alves Barbosa de Oliveira <164058997+GIOV4NN4EC@users.noreply.github.com>
2025-09-02 21:44:10 -0300

    Implemented delete_poll() and delete_post() functions

--- interface.py
+++ interface.py
@@ -1,32 +1,32 @@
 import services as sv
 
 def manage_player_recruitment():
-    print("\nRECRUITS MANAGEMENT")
+    print("\nRecruits Management")
     while True:
-        print("1. ADD RECRUIT")
-        print("2. LIST RECRUITS")
-        print("0. BACK")
-        escolha = input("CHOOSE: ")
+        print("1. Add Recruit")
+        print("2. List Recruits")
+        print("0. Back")
+        choice = input("Choose an option: ")
 
-        if escolha == "1":
+        if choice == "1":
             sv.add_recruit()
-        elif escolha == "2":
+        elif choice == "2":
             sv.list_recruits()
-        elif escolha == "0":
+        elif choice == "0":
             break
         else:
-            print("INVALID OPTION!")
+            print("Invalid option.")
 
 def manage_players():
     while True:
-        print("\nPLAYERS MANAGEMENT MENU")
-        print("1. ADD PLAYER")
-        print("2. LIST PLAYERS")
-        print("3. EDIT PLAYER")
-        print("4. REMOVE PLAYER")
-        print("5. MANAGE RECRUITS")
-        print("0. BACK")
-        option = input("CHOOSE: ")
+        print("\nPlayers Management Menu")
+        print("1. Add Player")
+        print("2. List Players")
+        print("3. Edit Player")
+        print("4. Remove Player")
+        print("5. Manage Recruits")
+        print("0. Back")
+        option = input("Choose an option: ")
 
         if option == "1":
             sv.add_player()
@@ -37,46 +37,42 @@ def manage_players():
         elif option == "4":
             sv.remove_player()
         elif option == "5":
-            sv.manage_player_recruitment()
+            manage_player_recruitment()
         elif option == "0":
             break
         else:
-            print("INVALID OPTION.")
-
+            print("Invalid option.")
 
 def team_management():
     while True:
-        print("\nTEAM MANAGEMENT MENU")
-        print("1. MANAGE PLAYERS")
-        print("2. PERFORMANCE TRACKING")
-        print("3. HEALTH MONITORING")
-        print("4. MANAGE RECRUITS")
-        print("0. BACK")
-        option = input("CHOOSE: ")
+        print("\nTeam Management Menu")
+        print("1. Manage Players")
+        print("2. Performance Tracking")
+        print("3. Health Monitoring")
+        print("0. Back")
+        option = input("Choose an option: ")
+        
         if option == "1":
             manage_players()
         elif option == "2":
             sv.performance_tracking()
         elif option == "3":
-            sv.health_monitoring()
-        elif option == "4":
-            sv.manage_player_recruitment()
+            health_monitoring()
         elif option == "0":
             break
         else:
-            print("INVALID OPTION.")
-
+            print("Invalid option.")
 
 def schedule_event():
     while True:
-        print("\nEVENTS MANAGEMENT MENU")
-        print("1. SCHEDULE MATCH")
-        print("2. SCHEDULE TRAINING")
-        print("3. LIST EVENTS")
-        print("4. REMOVE EVENT")
-        print("5. ADD MATCH RESULT")
-        print("0. BACK")
-        option = input("CHOOSE: ")
+        print("\nEvents Management Menu")
+        print("1. Schedule Match")
+        print("2. Schedule Training")
+        print("3. List Events")
+        print("4. Remove Event") 
+        print("5. Add Match Result")
+        print("0. Back")
+        option = input("Choose an option: ")
 
         if option == "1":
             sv.schedule_match()
@@ -85,54 +81,53 @@ def schedule_event():
         elif option == "3":
             sv.list_events()
         elif option == "4":
-            sv.delete_event()
+             sv.delete_event() 
         elif option == "5":
-            sv.register_result()
+             sv.register_result()
         elif option == "0":
             break
         else:
-            print("INVALID OPTION.")
+            print("Invalid option.")
 
 def health_monitoring():
     while True:
-        print("1. LIST INJURIED PLAYERS")
-        print("0. BACK")
-
-        option = input("CHOOSE AN OPTION: ").strip()
+        print("\nHealth Monitoring")
+        print("1. List Injured Players")
+        print("0. Back")
+        option = input("Choose an option: ").strip()
 
         if option == "1":
-            sv.list_injuried_players()
+            sv.list_injured_players()
         elif option == "0":
             break
         else:
-            print("INVALID OPTION.")
-
+            print("Invalid option.")
 
 def manage_equipments():
-    print("\nEQUIPMENT MANAGEMENT")
+    print("\nEquipment Management")
     while True:
-        print("1. ADD EQUIPMENT")
-        print("2. LIST EQUIPMENTS")
-        print("0. BACK")
-        escolha = input("CHOOSE: ")
+        print("1. Add Equipment")
+        print("2. List Equipment")
+        print("0. Back")
+        choice = input("Choose an option: ")
 
-        if escolha == "1":
+        if choice == "1":
             sv.add_equipment()
-        elif escolha == "2":
+        elif choice == "2":
             sv.list_equipments()
-        elif escolha == "0":
+        elif choice == "0":
             break
         else:
-            print("INVALID OPTION!")
+            print("Invalid option.")
 
 def manage_finances():
-    print("\nFINANCIAL MANAGEMENT")
+    print("\nFinancial Management")
     while True:
-        print(f"CURRENT BALANCE: $ {sv.account.balance:.2f}")
-        print("1. REGISTER INCOME")
-        print("2. REGISTER EXPENSE")
-        print("0. BACK")
-        option = input("CHOOSE: ")
+        print(f"\nCurrent Balance: $ {sv.data.account.balance:.2f}")
+        print("1. Register Income")
+        print("2. Register Expense")
+        print("0. Back")
+        option = input("Choose an option: ")
 
         if option == "1":
             sv.register_income()
@@ -141,17 +136,19 @@ def manage_finances():
         elif option == "0":
             break
         else:
-            print("INVALID OPTION!")
+            print("Invalid option.")
 
 def media_and_social():
-    print("\nMEDIA AND PUBLIC RELATIONS")
+    print("\nMedia and Public Relations")
     while True:
-        print("1. CREATE POLL")
-        print("2. CREATE POST")
-        print("3. LIST POLLS")
-        print("4. LIST POSTS")
-        print("0. BACK")
-        option = input("CHOOSE: ")
+        print("1. Create Poll")
+        print("2. Create Post")
+        print("3. List Polls")
+        print("4. List Posts")
+        print("5. Delete Poll")
+        print("6. Delete Post")
+        print("0. Back")
+        option = input("Choose an option: ")
 
         if option == "1":
             sv.create_poll()
@@ -161,23 +158,26 @@ def media_and_social():
             sv.list_polls()
         elif option == "4":
             sv.list_posts()
+        elif option == "5":
+            sv.delete_poll()
+        elif option == "6":
+            sv.delete_post()
         elif option == "0":
             break
         else:
-            print("INVALID OPTION!")
+            print("Invalid option.")
 
 def main_menu():
     while True:
-        print("\n-----SPORTS TEAM MANAGEMENT APP-----")
-        print(" SELECT AN OPTION:")
-        print("1. MANAGE TEAM AND PLAYERS")
-        print("2. MANAGE MATCHES AND TRAININGS")
-        print("3. MANAGE EQUIPMENT")
-        print("4. MANAGE FINANCES")
-        print("5. MEDIA AND SOCIAL")
-        print("0. EXIT")
-
-        option = input("")
+        print("\n----- Sports Team Management App -----")
+        print("Select an option:")
+        print("1. Manage Team and Players")
+        print("2. Manage Matches and Training")
+        print("3. Manage Equipment")
+        print("4. Manage Finances")
+        print("5. Media and Social")
+        print("0. Exit")
+        option = input("Choose an option: ")
 
         if option == "1":
             team_management()
@@ -190,7 +190,7 @@ def main_menu():
         elif option == "5":
             media_and_social()
         elif option == "0":
-            print("CLOSING SYSTEM.")
+            print("Closing system.")
             break
         else:
-            print("INVALID OPTION!")
+            print("Invalid option.")
